f3365a4e47ce53458d4bde208b89d89df25264ad
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-08-31 16:33:30 -0500

    Feature: New Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DsmcodeApi::Application.config.secret_key_base = '4088ce06d8f85549d4dcf4a2f660cf5a26482c231040dc79eb9d1e36ebd0d70d49b4907f58708e917ecc5319bf9e53b68286347875f00e6a21cc6c589ae7ecee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DsmcodeApi::Application.config.session_store :cookie_store, key: '_dsmcode-api_session'
